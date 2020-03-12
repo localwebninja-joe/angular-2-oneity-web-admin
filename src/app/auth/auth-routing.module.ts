39882255669db2f2d31e33be28681a9636cdce35
Joe Marie Lui <netzone.joe@gmail.com>
2020-03-13 01:37:54 +0800

    delete this code

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -5,20 +5,6 @@ import { LoginPageComponent, RegisterPageComponent } from '@app/auth/containers'
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent, data: { title: 'Login' } },
   { path: 'register', component: RegisterPageComponent, data: { title: 'Register' } },
-  { path: 'join', component: RegisterPageComponent, data: { title: 'Join' } },
-  // This is what I meant you to do. so for now I just use the RegisterPageComponent so that the 404 will not display
-  // You need to do this steps:
-  // Replace the RegisterPageComponent to JoinPageComponent
-  // You need to create JoinPageComponent component to handle the http client request 
-  // you will need to get the ref value
-  /**
-   * const urlParams = new URLSearchParams(window.location.search);
-   * const myParam = urlParams.get('ref'); // the value is binomical
-   */
-  // once you get the value you have to dispatch with this url
-  // https://svc.binomical.com/join?ref={myParam}
-  // after request is done you will force the redirect to url
-  // https://svc.binomical.com/register
 ];
 
 @NgModule({
